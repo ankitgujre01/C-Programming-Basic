91acb971bc88c46cfb34359d9a7ebf4db647818c
Ankit gujre <144484381+ankitgujre01@users.noreply.github.com>
2024-10-02 10:50:42 +0530

    Add files via upload

---
+++ DataTypes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a = 10;  //integer data type
+    float b = 10.10;  //float datatype
+    char ch = 'a';  //character data type
+
+    printf("%d \n",a);   // \n for line break
+    printf("%f \n",b);
+    printf("%c",ch);
+}
