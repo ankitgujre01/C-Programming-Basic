22849fdc223a70eedd1f0ae4e693cee34f194d6c
Ankit gujre <144484381+ankitgujre01@users.noreply.github.com>
2024-10-03 10:57:00 +0530

    Add files via upload

---
+++ VolumeOfCylinder.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    float radius, height;
+
+    printf("Enter Radius:\n");
+    scanf("%f", &radius);
+
+    printf("Enter Height:\n");
+    scanf("%f", &height);
+
+    // volume of cylinder V = 2*3.14*r^2*h
+    printf("Volume of Cylinder = %f", 2 * 3.14 * radius * radius * height);
+    return 0;
+}
