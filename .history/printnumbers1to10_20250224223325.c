6f046c9e91fafa5ce497ac7b3ec2d799e2612e03
ankitgujre <kumarankit65494@gmail.com>
2025-03-14 10:29:51 +0530

    add code

--- .history/printnumbers1to10_20250224223325.c
+++ .history/printnumbers1to10_20250224223325.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void main(){   // void main() is also correct here because the return type of main function is int by default and the return type of main function is not void by default. 
+void main(){          // void main() is also correct here because the return type of main function is int
     // for(int i=1; i<=10; i++){
     //     printf("%d\n", i);
     // }
